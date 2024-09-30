0b37bd4bd70f4596f570d21fb91ad5858a5c2d3f
riteemo <irina.qqu@gmail.com>
2024-09-30 22:50:05 +0300

    Receptionist class deleted

--- Receptionist.py
+++
@@ -1,6 +0,0 @@
-from HospitalInhabitant import HospitalInhabitant
-
-class Receptionist(HospitalInhabitant):
-    def __init__(self, name: str, surname: str, phone: str, work_id: int):
-        super().__init__(name, surname, phone)
-        self.work_id = work_id
