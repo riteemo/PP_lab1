02c6ef1f54d6acb009414b26c578be29782d905e
riteemo <irina.qqu@gmail.com>
2024-09-30 22:34:50 +0300

    Cleaner class added

---
+++ Cleaner.py
@@ -0,0 +1,6 @@
+from HospitalInhabitant import HospitalInhabitant
+
+class Cleaner(HospitalInhabitant):
+    def __init__(self, name: str, surname: str, phone: str, floor : int):
+        super().__init__(name, surname, phone)
+        self.floor = floor
