6bdc0b1f059da40abbf7e07dc7b69fb9b9cb660e
riteemo <irina.qqu@gmail.com>
2024-09-30 22:40:00 +0300

    Visitor class added

---
+++ Visitor.py
@@ -0,0 +1,6 @@
+from HospitalInhabitant import HospitalInhabitant
+
+class Visitor(HospitalInhabitant):
+    def __init__(self, name: str, surname: str, phone: str, patient_id: int):
+        super().__init__(name, surname, phone)
+        self.patient_id = patient_id
